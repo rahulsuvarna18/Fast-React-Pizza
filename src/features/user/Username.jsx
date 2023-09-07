3098499957a846c2214afb707f581836afc61225
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-09-07 18:05:35 +0200

    added flexbox to appLayout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Rahul</div>;
+  return <div className="hidden text-sm font-semibold md:block">Rahul</div>;
 }
 
 export default Username;
